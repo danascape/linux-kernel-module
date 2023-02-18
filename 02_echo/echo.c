50d2f5005f27f4027e47cc55c101ec53af3d31a0
danascape <danascape@gmail.com>
2023-02-19 03:27:08 +0530

    echo: Input string as a parameter and give output
    Signed-off-by: danascape <danascape@gmail.com>

--- 02_echo/echo.c
+++ 02_echo/echo.c
@@ -1,14 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0
 #include<linux/module.h>
+#include<linux/moduleparam.h>
 #include<linux/init.h>
 
 /*
  * A simple echo module
  */
 
+static char *string = "Default";
+
+module_param(string, charp, 0000);
+MODULE_PARM_DESC(string, "A character string");
+
 static int __init echo_init(void)
 {
 	pr_info("echo: Initialise... \n");
+	pr_info("The provided string: %s\n", string);
 	return 0;
 }
 
